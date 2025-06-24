ceaebd727b97f6b1a8f6bf7d2a532f07282d6bcf
Ahmed Ramadan <=>
2025-06-24 19:30:10 +0300

    adding validation to user

--- src/main/java/com/ShopApp/E_Commerce/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ShopApp/E_Commerce/exceptions/GlobalExceptionHandler.java
@@ -1,20 +1,29 @@
 package com.ShopApp.E_Commerce.exceptions;
 
 import com.ShopApp.E_Commerce.response.ApiResponse;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.nio.file.AccessDeniedException;
 import java.util.Collections;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<ApiResponse> handleAccessDeniedException(Exception e){
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ApiResponse(e.getMessage(), Collections.EMPTY_MAP));
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse> handleValidationException(MethodArgumentNotValidException e){
+        String errorMessage = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ApiResponse(errorMessage, Collections.EMPTY_MAP));
     }
 }
--- src/main/java/com/ShopApp/E_Commerce/request/CreateUserRequest.java
+++ src/main/java/com/ShopApp/E_Commerce/request/CreateUserRequest.java
@@ -1,12 +1,18 @@
 package com.ShopApp.E_Commerce.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
-
 public class CreateUserRequest {
+    @NotBlank(message = "First name mustn't be empty")
     private String firstName;
+    @NotBlank(message = "Last name mustn't be empty")
     private String lastName;
+
+    @NotBlank(message = "Email mustn't be empty")
     private String email;
+
+    @NotBlank(message = "Password mustn't be empty")
     private String password;
 }
